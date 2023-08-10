dab0b156d9efdddc5ca5b4f17809036f21dfe785
Marcelle Vargas <marcelle.vargas96@gmail.com>
2023-08-10 17:29:25 -0300

    feat: first navbar component

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,20 +1,41 @@
-import React from 'react'
-import "./navbar.scss"
+import React from "react";
+import "./navbar.scss";
+import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <div className='navbar'>
-      <div className="logo">
-        BLOOM&BOOM
+    <div className="navbar">
+      <div className="header-nav">
+        <div className="logo">BLOOM&BOOM</div>
+        <div className={`links ${showMenu ? "show-menu" : "hidden-menu"}`}>
+          <a href="http://" className="link">
+            About us
+          </a>
+          <a href="http://" className="link">
+            Catalog
+          </a>
+          <a href="http://" className="link">
+            Delivery
+          </a>
+          <a href="http://" className="link">
+            Reviews
+          </a>
+          <a href="http://" className="link">
+            Contacts
+          </a>
+        </div>
       </div>
-      <div className="links">
-        <a href="http://">About us</a>
-        <a href="http://">Catalog</a>
-        <a href="http://">Delivery</a>
-        <a href="http://">Reviews</a>
-        <a href="http://">Contacts</a>
+      <div className="language">
+        <span>ENG</span>
+        <AiOutlineMenu onClick={openMenu} className="responsive-icon" />
       </div>
-      <div className="language">ENG</div>
     </div>
-  )
+  );
 }
